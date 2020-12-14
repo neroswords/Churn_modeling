a8452c172d6c03b31ae2723cff74e913bdc3b43e
neroswords <tanathep.min@gmail.com>
2020-12-14 20:54:34 +0700

    first commit

---
+++ Project/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, abort, render_template
+import json
+import pickle
+# from Project.Config import *
+
+
+# client = pymongo.MongoClient("")
+# db = client.test
+# col = db["User"]
+
+app = Flask(__name__)
+dt_model=pickle.load(open('dt_model.pkl','rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('landing.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    # print(int_features)
+    # print(final)
+    # prediction=model.predict_proba(final)
+    output='{0:.{1}f}'.format(prediction[0][1], 2)
+
+    if output>str(0.5):
+        return render_template('forest_fire.html',pred='Your Forest is in Danger.\nProbability of fire occuring is {}'.format(output),bhai="kuch karna hain iska ab?")
+    else:
+        return render_template('forest_fire.html',pred='Your Forest is safe.\n Probability of fire occuring is {}'.format(output),bhai="Your Forest is Safe for now")
