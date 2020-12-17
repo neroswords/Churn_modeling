9bcb9cce60f66898995be8949323094eb0143470
neroswords <tanathep.min@gmail.com>
2020-12-17 16:06:49 +0700

    fix

--- Project/Config/predict.py
+++ Project/Config/predict.py
@@ -12,13 +12,10 @@ import warnings
 import pickle
 
 warnings.filterwarnings("ignore")
-# models = []
 
 data = pd.read_csv("./Project/Config/churn.csv")
 features = data[data.columns[data.columns!='Exited'] ]
 target = data[data.columns[data.columns=='Exited'] ]
-# x = data[1:, 1:-1]
-# y = data[1:, -1]
 x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=0)
 
 sc = StandardScaler()
@@ -28,11 +25,10 @@ x_test_std  = sc.transform(x_test)
 
 dt = DecisionTreeClassifier(criterion="entropy")
 dt.fit(x_train_std, y_train)
-# dt_predict = dt.predict(x_test)
-# print(x_test)
+
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(x_train_std, y_train)
-# knn_pred = knn.predict(x_test_std)
+
 
 rfc = RandomForestClassifier(n_estimators=15, max_depth=None,
     min_samples_split=2, random_state=0)
@@ -40,33 +36,14 @@ rfc.fit(x_train_std, y_train)
 
 mlp = MLPClassifier(solver='lbfgs', hidden_layer_sizes=(10,), random_state=0)
 mlp.fit(x_train_std, y_train)
-# nn_predict = mlp.predict(x_test_std)
 
 nb = GaussianNB()
 nb.fit(x_train_std, y_train)
 NB_pred = nb.predict(x_test_std)
 
-# models.append(('DecisionTree', dt))
-# models.append(('KNeighbors', knn))
-# models.append(('RandomForest', rfc))
-# models.append(('MLP', mlp))
-# models.append(('NavieBayes', nb))
-
-# b = log_reg.predict_proba(final)
-
-# with open("models.pckl", "wb") as f:
-#     for model in models:
-#          pickle.dump(model, f)
 
 pickle.dump(dt,open('Project/Config/dt_model.pkl','wb'))
 pickle.dump(mlp,open('Project/Config/mlp_model.pkl','wb'))
 pickle.dump(knn,open('Project/Config/knn_model.pkl','wb'))
 pickle.dump(nb,open('Project/Config/nb_model.pkl','wb'))
 pickle.dump(rfc,open('Project/Config/rfc_model.pkl','wb'))
-
-# model=pickle.load(open('dt_model.pkl','rb'))
-
-
-# def dt_predict(self):
-#     dt_pred = dt.predict(self)
-#     pass dt_pred
